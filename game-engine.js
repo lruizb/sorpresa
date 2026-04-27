a62e1c703f6837c594ec9bbbb40b87ea15b9aabd
Luis Ruiz <luiscaradepizo123456789@gmail.com>
2026-04-27 08:42:41 -0500

    Update game-engine.js

--- game-engine.js
+++ game-engine.js
@@ -1762,9 +1762,9 @@ function redimensionarCanvas() {
   canvas.height = window.innerHeight;
   
   joystick.centerX = canvas.width * 0.15;
-  joystick.centerY = canvas.height * 0.85;
+  joystick.centerY = canvas.height * 0.75;
   jumpButton.x = canvas.width * 0.85;
-  jumpButton.y = canvas.height * 0.85;
+  jumpButton.y = canvas.height * 0.75;
   
   groundY = canvas.height - 100;
 }
